0f4adf426c15c48001bd7eb7909b0b8097092b9f
Valeria Voronova <66879713+valeriavoronova@users.noreply.github.com>
2022-06-05 14:49:13 +0100

    02 tries

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,5 @@
 // // commonjs
 // const flatpickr = require("flatpickr");
-9
 // // es modules are recommended, if available, especially for typescript
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
@@ -16,25 +15,39 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-      console.log(selectedDates[0]);
-      //const curDate = this.defaultDate.getTime();
-      console.log(this.defaultDate);
+      //console.log(selectedDates[0]);
+      // const curDate = this.defaultDate;
+      // console.log("def",this.defaultDate);
       if (selectedDates[0] < new Date()) {
           refs.btnStart.disabled = true;
           window.alert("Please choose a date in the future");
           return;
       }
-      refs.btnStart.disabled = false;
+    refs.btnStart.disabled = false;
+    return selectedDates[0];
   },
 };
 
+const timer = {
+  start() {
+    const curTime = options.defaultDate;
+    //const selectedTime;
+      //const timeToCount = selectedTime - curTime;
+    setInterval(() => {
+      console.log(convertMs(curTime), "133");
+    }, 1000);
+     // return console.log(convertMs(curTime), "133");
+  }
+};
+
+timer.start();
 const flat = new flatpickr(refs.timePick, options);
 
 refs.btnStart.addEventListener('click', convertMs);
 
-timeChangeId = setInterval(() => {
-    document.querySelector('span[data-days]').textContent -= 1;
- }, 1000);
+// const timeChangeId = setInterval(() => {
+//     //document.querySelector('span[data-days]').textContent -= 1;
+//  }, 1000);
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
